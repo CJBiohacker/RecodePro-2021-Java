a0e67a093dd87b2ad6c7edeeece9b9966e4169dc
CJBiohacker <cj.moyses@gmail.com>
2021-10-19 09:29:29 -0300

    "Programação Orientada à Objetos - Herança"

---
+++ Java/src/POO/Construtor/AtividadePraticaHeranca/Voto.java
@@ -0,0 +1,11 @@
+package POO.Construtor.AtividadePraticaHeranca;
+
+class Voto {
+
+    public static void main(String[] args) {
+
+        Aluno cj = new Aluno("Carlos Junior",31);
+
+        System.out.println(cj.votar());
+    }
+}
